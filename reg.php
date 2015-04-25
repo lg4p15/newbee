026defc9737e536370c961030b31b7d681225478
lg4p15 <15592164758@163.com>
2015-04-25 21:49:33 +0800

    lg4p15

--- reg.php
+++ reg.php
@@ -2,21 +2,32 @@
 header("Content-type: text/html; charset=utf-8");
 $user='treenewbee';
 $pwd='shuxinfeng';
-$con = mysql_connect('localhost', $user, $pwd) or die("<script>window.location='error.html'</script>");	
-mysql_select_db('tree_newbee') or die("<script>window.location='error.html'</script>");
-mysql_query("set character set 'utf8'");
-	$name = $_POST['id'];
-	$userpwd=$_POST['pwd'];
-	if(!ereg("^[_a-z0-9A-Z]+$",$name))die("<script>window.location='ban.html'</script>");
-	if(!ereg("^[_a-z0-9A-Z]+$",$userpwd))die("<script>window.location='ban.html'</script>");
-	$re=mysql_query("select id from user where id = '$name'" );
-	echo "ok";
-	if(mysql_fetch_array($re)){
-		echo "<script>window.location='rep.html'</script>";
+if($con = mysql_connect('localhost', $user, $pwd)){
+	if(mysql_select_db('tree_newbee')){
+		mysql_query("set character set 'utf8'");
+		$name = $_POST['id'];
+		$userpwd=$_POST['pwd'];
+		if(!ereg("^[_a-z0-9A-Z]+$",$name)) echo "<script>window.location='ban.html'</script>";
+		else if(!ereg("^[_a-z0-9A-Z]+$",$userpwd)) echo "<script>window.location='ban.html'</script>";
+		else{
+			$re=mysql_query("select id from user where id = '$name'" );
+			echo "ok";
+			if(mysql_fetch_array($re)){
+				echo "<script>window.location='rep.html'</script>";
+			}else{
+				$str="insert into user (id,pwd) values('$name','$userpwd')";
+				if(mysql_query($str)){
+					echo "<script>window.location='lgscs.html'</script>";
+				}else{
+					echo "<script>window.location='error.html'</script>";
+				}
+			}
+		}
 	}else{
-		$str="insert into user (id,pwd) values('$name','$userpwd')";
-		mysql_query($str)or die("<script>window.location='error.html'</script>");
-		echo "<script>window.location='lgscs.html'</script>";
+		 echo "<script>window.location='error.html'</script>";
 	}
 	mysql_close($con);
+}else{
+	echo "<script>window.location='error.html'</script>";
+}
 ?>
--- user.php
+++ user.php
@@ -1,35 +1,49 @@
-<?php
+	<?php
 header("Content-type: text/html; charset=utf-8");
 $user='treenewbee';
 $pwd='shuxinfeng';
-$con = mysql_connect('localhost', $user, $pwd) or die("<script>window.location='error.html'</script>");	
-mysql_select_db('tree_newbee') or die("<script>window.location='error.html'</script>");
-mysql_query("set character set 'utf8'");
-if(isset($_POST['name'])){ 
-	$name = $_POST['name'];
-	$pwd=$_POST['pwd'];
-	if(empty($name))die("<script>window.location='ban.html'</script>");
-	if(empty($pwd))die("<script>window.location='pwd.html'</script>");
-	if(!ereg("^[_0-9a-zA-Z]+$",$name))die("<script>window.location='ban.html'</script>");
-	if(!ereg("^[_0-9a-zA-Z]+$",$pwd))die("<script>window.location='ban.html'</script>");
+if($con = mysql_connect('localhost', $user, $pwd)){
+	if(mysql_select_db('tree_newbee')){
+		mysql_query("set character set 'utf8'");
+		if(isset($_POST['name'])){
+			$name = $_POST['name'];
+			$pwd=$_POST['pwd'];
+			if(empty($name)) echo "<script>window.location='ban.html'</script>";
+			else if(empty($pwd)) echo "<script>window.location='pwd.html'</script>";
+			else if(!ereg("^[_0-9a-zA-Z]+$",$name)) echo "<script>window.location='ban.html'</script>";
+			else if(!ereg("^[_0-9a-zA-Z]+$",$pwd)) echo "<script>window.location='ban.html'</script>";
 //这里还需要对密码进行加密，明文存储有风险
-	$res=mysql_query("select id from user where id = '$name' and pwd= '$pwd' limit 1")or die("<script>window.location='error.html'</script>");
-	if(mysql_fetch_array($res)){
-	setcookie("user", $name, time()+3600);
-	echo "<script target='_top'>window.location='index.html'</script>";
-	}else{
-	echo "<script>window.location='pwd.html'</script>";
-	}
-}else{
-	$ck=$_COOKIE['user'];
+			else if($res=mysql_query("select id from user where id = '$name' and pwd= '$pwd' limit 1")){
+				if(mysql_fetch_array($res)){
+				setcookie("user", $name, time()+3600);
+				echo "<script target='_top'>window.location='index.html'</script>";
+				}else{
+				echo "<script>window.location='pwd.html'</script>";
+				}
+			}else{
+				echo "<script>window.location='error.html'</script>";
+			}
+		}else{
+			$ck=$_COOKIE['user'];
 //这里也是要cookie加密
-	if(!empty($ck)&&!ereg("^[_0-9a-zA-Z]+$",$ck))die("<script>window.location='ban.html'</script>");
-	$res=mysql_query("select id from user where id='".$ck."' limit 1;")or die("<script>window.location='error.html'</script>");
-	if(mysql_fetch_array($res)){
-		echo "<script>window.location='lguser.html'</script>";
+			if(!empty($ck)&&!ereg("^[_0-9a-zA-Z]+$",$ck)) echo "<script>window.location='ban.html'</script>";
+			else{
+				if($res=mysql_query("select id from user where id='".$ck."' limit 1;")){
+					if(mysql_fetch_array($res)){
+						echo "<script>window.location='lguser.html'</script>";
+					}else{
+						echo "<script>window.location='user.html'</script>";
+					}
+				}else{
+					echo "<script>window.location='error.html'</script>";
+				}
+			}
+		}
 	}else{
-	echo "<script>window.location='user.html'</script>";
+		echo "<script>window.location='error.html'</script>";
 	}
+	mysql_close($con);
+}else{
+	echo "<script>window.location='error.html'</script>";	
 }
-mysql_close($con);
 ?>
