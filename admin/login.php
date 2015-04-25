19775990f27f9e81c7599369b07c537c88ae1282
lg4p15 <15592164758@163.com>
2015-04-25 08:07:48 +0800

    lg4p15

--- admin/login.php
+++ admin/login.php
@@ -6,6 +6,8 @@ mysql_select_db('tree_newbee') or die("<script>window.location='error.html'</scr
 if(isset($_POST['name'])){ 
 	$name = $_POST['name'];
 	$pwd=$_POST['pwd'];
+	if(empty($name))die("<script>window.location='noname.html'</script>");
+	if(empty($pwd))die("<script>window.location='pwd.html'</script>");
 	if(!ereg("^[_0-9a-zA-Z]+$",$name))die("<script>window.location='ban.html'</script>");
 	if(!ereg("^[_0-9a-zA-Z]+$",$pwd))die("<script>window.location='ban.html'</script>");
 //这里还需要对密码进行加密，明文存储有风险
@@ -19,7 +21,7 @@ if(isset($_POST['name'])){
 }else{
 	$ck=$_COOKIE['admin'];
 //这里也是要cookie加密
-	if(!ereg("^[_0-9a-zA-Z]+$",$ck))die("<script>window.location='ban.html'</script>");
+	if(!empty($ck)&&!ereg("^[_0-9a-zA-Z]+$",$ck))die("<script>window.location='ban.html'</script>");
 	$res=mysql_query("select name from su where name='".$ck."' limit 1;")or die("<script>window.location='error.html'</script>");
 	if(mysql_fetch_array($res)){
 		echo "success";
